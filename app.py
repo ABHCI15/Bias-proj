bd21e3a680fc49891fbc1426d4231c27f5bc3355
Abhiram <113409577+ABHCI15@users.noreply.github.com>
2025-01-22 20:14:46 -0800

    css change 2.0

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ import random
 
 # Create GSheets connection
 conn = st.connection("gsheets", type=GSheetsConnection)
-css = """#stAppHeader st-emotion-cache-h4xjwg e10jh26i0 {
-  visibility: hidden;
-}"""
-
-st.markdown(css, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
