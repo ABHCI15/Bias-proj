c5caa2e758779c3427ea7d72d8c881e309e7363f
Abhiram <113409577+ABHCI15@users.noreply.github.com>
2025-01-22 20:15:21 -0800

    remove css

--- app.py
+++ app.py
@@ -5,18 +5,7 @@ import random
 
 # Create GSheets connection
 conn = st.connection("gsheets", type=GSheetsConnection)
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
 
 
